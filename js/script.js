3421edee42d2cedfe6f22c657d28161a9925c726
Letitia-Buzea <buzea.elena@gmail.com>
2021-10-10 22:34:38 +0300

    Update script.js
    -Added property tags to quotes array
    -Added the randomRGB function

--- js/script.js
+++ js/script.js
@@ -15,70 +15,80 @@ const quotes = [
     quote: `All the world's a stage, and all the men and women merely players.`,
     source: 'William Shakespeare',
     citation: 'As You Like It',
-    year: 1623
+    year: 1623,
+    tags: 'Poetry'
   },
 
   {
     quote: `Our lives are not our own. We are bound to others, past and present, and by each crime and every kindness, we birth our future.`,
     source: 'David Mitchell',
     citation: 'Cloud Atlas',
-    year: 2004
+    year: 2004,
+    tags: 'Philosophical'
   },
 
   {
     quote: `You must take life the way it comes at you and make the best of it.`,
     source: 'Yann Martel',
     citation: 'Life of Pi',
-    year: 2001
+    year: 2001,
+    tags: 'Inspiring'
   },
 
   {
     quote: `Those who wish to learn magic ought to begin by looking around them.`,
     source: 'Paulo Coelho',
     citation: 'Hippie',
-    year: 2018
+    year: 2018,
+    tags: 'Inspiring'
   },
 
   {
     quote: `Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.`,
     source: 'Rumi',
     citation: `Masnavi-I Ma'navi`,
-    year: 1260
+    year: 1260,
+    tags: 'Philosophical'
   },
 
   {
     quote: `You were born with wings, why prefer to crawl through life?`,
     source: 'Rumi',
     citation: `Masnavi-I Ma'navi`,
-    year: 1260
+    year: 1260,
+    tags: 'Motivational'
   },
 
   {
     quote: `All we have to decide is what to do with the time that is given to us.`,
     source: 'J.R.R. Tolkien',
     citation: 'Lord of the Rings',
-    year: 1954
+    year: 1954,
+    tags: 'Motivational'
   },
 
   {
     quote: `Every path is the right path. Everything could've been anything else. And it would have just as much meaning.`,
     source: 'Nemo',
     citation: 'Mr. Nobody',
-    year: 2009
+    year: 2009,
+    tags: 'Philosophical'
   },
 
   {
     quote: `Just because I'm losing, doesn't mean I'm lost.`,
     source: 'Coldplay',
     citation: 'Lost',
-    year: 2008
+    year: 2008,
+    tags: 'Motivational'
   },
 
   {
     quote: `Our lives are difined by opportunities, even the ones we miss.`,
     source: 'Benjamin Button',
     citation: 'The Curious Case of Benjamin Button',
-    year: 2009
+    year: 2009,
+    tags: 'Philosophical'
   }
 ];
 
@@ -99,9 +109,11 @@ function getRandomQuote() {
 /***
  * `printQuote` function
 ***/
+let html = '';
+
 function printQuote() {
  let randomQuote = getRandomQuote();
- let html = `
+ html = `
   <p class="quote"> ${randomQuote.quote} </p>
   <p class="source"> ${randomQuote.source}
  `;
@@ -111,9 +123,26 @@ function printQuote() {
   if ( randomQuote.year !== undefined ) {
     html += `<span class="year"> ${randomQuote.year} </span>`
   };
+  if ( randomQuote.tags !== undefined ) {
+    html += `<span class="year"> ${randomQuote.tags} </span>`
+  };
   html += `</p>`;
   document.getElementById('quote-box').innerHTML = html ;
 };
+
+//randomRGB function - creates a random color
+const randomValue = () => Math.floor( Math.random*256 );
+
+function randomRGB() {
+  const color = `rgb( ${randomValue()}, ${randomValue()}, ${randomValue()} )`;
+  color;
+}
+
+
+
+
+
+
 
 
 
