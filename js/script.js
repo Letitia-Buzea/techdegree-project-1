ef322a21d531bc0d262e62f3e522f227214ea6c4
Letitia-Buzea <buzea.elena@gmail.com>
2021-10-10 21:00:35 +0300

    Update script.js
    -Added an array named quotes.
    -Added getRandomQuote function
    -Added printQuote function

--- js/script.js
+++ js/script.js
@@ -10,18 +10,110 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {
+    quote: `All the world's a stage, and all the men and women merely players.`,
+    source: 'William Shakespeare',
+    citation: 'As You Like It',
+    year: 1623
+  },
+
+  {
+    quote: `Our lives are not our own. We are bound to others, past and present, and by each crime and every kindness, we birth our future.`,
+    source: 'David Mitchell',
+    citation: 'Cloud Atlas',
+    year: 2004
+  },
+
+  {
+    quote: `You must take life the way it comes at you and make the best of it.`,
+    source: 'Yann Martel',
+    citation: 'Life of Pi',
+    year: 2001
+  },
+
+  {
+    quote: `Those who wish to learn magic ought to begin by looking around them.`,
+    source: 'Paulo Coelho',
+    citation: 'Hippie',
+    year: 2018
+  },
+
+  {
+    quote: `Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.`,
+    source: 'Rumi',
+    citation: `Masnavi-I Ma'navi`,
+    year: 1260
+  },
+
+  {
+    quote: `You were born with wings, why prefer to crawl through life?`,
+    source: 'Rumi',
+    citation: `Masnavi-I Ma'navi`,
+    year: 1260
+  },
+
+  {
+    quote: `All we have to decide is what to do with the time that is given to us.`,
+    source: 'J.R.R. Tolkien',
+    citation: 'Lord of the Rings',
+    year: 1954
+  },
+
+  {
+    quote: `Every path is the right path. Everything could've been anything else. And it would have just as much meaning.`,
+    source: 'Nemo',
+    citation: 'Mr. Nobody',
+    year: 2009
+  },
+
+  {
+    quote: `Just because I'm losing, doesn't mean I'm lost.`,
+    source: 'Coldplay',
+    citation: 'Lost',
+    year: 2008
+  },
+
+  {
+    quote: `Our lives are difined by opportunities, even the ones we miss.`,
+    source: 'Benjamin Button',
+    citation: 'The Curious Case of Benjamin Button',
+    year: 2009
+  }
+];
+
 
 
 
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote() {
+  let randomNumber = Math.floor( Math.random() * 10 );
+  let randomQuote = quotes[randomNumber];
+  return randomQuote;
+};
 
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+ let randomQuote = getRandomQuote();
+ let html = `
+  <p class="quote"> ${randomQuote.quote} </p>
+  <p class="source"> ${randomQuote.source}
+ `;
+  if( randomQuote.citation !== undefined ) {
+    html += `<span class="citation"> ${randomQuote.citation} </span>`;
+  };
+  if ( randomQuote.year !== undefined ) {
+    html += `<span class="year"> ${randomQuote.year} </span>`
+  };
+  html += `</p>`;
+  document.getElementById('quote-box').innerHTML = html ;
+};
 
 
 
